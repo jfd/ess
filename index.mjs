54634f14284213b695a51faaae0f12dde66f8b2b
Johan <dahlberg.johan@gmail.com>
2020-06-23 16:12:06 +0200

    Initial commit

---
+++ index.mjs
@@ -0,0 +1,4 @@
+import * as Dict from "./src/Dict.mjs"; export {Dict};
+import * as List from "./src/List.mjs"; export {List};
+import * as Path from "./src/Path.mjs"; export {Path};
+import * as Str from "./src/Str.mjs"; export {Str};
